b9f321030515c5a4db8162c0864dac24f197ae0e
wsmo6 <m.otoofi@lboro.ac.uk>
2025-02-21 20:25:38 +0000

    jn added

---
+++ main.py
@@ -0,0 +1,15 @@
+import argparse
+import os
+import sys
+from frictionsegnet.Train import *
+import yaml
+
+
+def main():
+    f = open("config.yml", "r")
+    args = yaml.safe_load(f)
+    train(**args)
+
+
+if __name__ == "__main__":
+    main()
