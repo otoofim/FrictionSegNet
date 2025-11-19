4df42327661146949cba7bb26c48eb15eccb9760
Mohammad Otoofi <moh1371@gmail.com>
2025-11-19 20:13:09 +0330

    dynamic residual block

--- probabilistic_unet/model/residual_block.py
+++ probabilistic_unet/model/residual_block.py
@@ -1,38 +1,45 @@
-import torch
 import torch.nn as nn
 
 class ResidualBlock(nn.Module):
-    def __init__(self, in_channels, out_channels=None, stride=1, downsample=None):
+    def __init__(self, in_channels, out_channels=None, stride=1, downsample=None, num_convs=2, kernel_size=3, activation=nn.ReLU, norm_layer=nn.BatchNorm2d):
         super(ResidualBlock, self).__init__()
         self.out_channels = out_channels if out_channels is not None else in_channels
-        self.conv1 = nn.Conv2d(in_channels, self.out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(self.out_channels)
-        self.relu = nn.ReLU()
-        self.conv2 = nn.Conv2d(self.out_channels, self.out_channels, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(self.out_channels)
+        self.num_convs = num_convs
+        self.convs = nn.ModuleList()
+        self.norms = nn.ModuleList()
+        self.activation = activation()
         self.downsample = downsample
+
+        for i in range(num_convs):
+            conv_in_channels = in_channels if i == 0 else self.out_channels
+            conv_out_channels = self.out_channels
+            self.convs.append(nn.Conv2d(conv_in_channels, conv_out_channels, kernel_size=kernel_size, stride=(stride if i == 0 else 1), padding=kernel_size // 2, bias=False))
+            self.norms.append(norm_layer(conv_out_channels))
 
     def forward(self, x):
         identity = x
 
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
+        out = x
+        for conv, norm in zip(self.convs, self.norms):
+            out = conv(out)
+            out = norm(out)
+            out = self.activation(out)
 
         if self.downsample is not None:
             identity = self.downsample(x)
 
         out += identity
-        out = self.relu(out)
+        out = self.activation(out)
 
         return out
 
-# Example usage of ResidualBlock with downsampling
-# downsample = nn.Sequential(
-#     nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
-#     nn.BatchNorm2d(out_channels)
+# Example usage:
+# block = ResidualBlock(
+#     in_channels=64, 
+#     out_channels=128, 
+#     stride=2, 
+#     num_convs=3, 
+#     kernel_size=5, 
+#     activation=nn.LeakyReLU, 
+#     norm_layer=nn.GroupNorm
 # )
-# block = ResidualBlock(in_channels, out_channels, stride, downsample)
