6a1efe8a2e65b1c4aa731ae467ae6e78806272f7
Mohammad Otoofi <moh1371@gmail.com>
2025-11-19 20:06:17 +0330

    residual blocks refactored

--- probabilistic_unet/model/residual_block.py
+++ probabilistic_unet/model/residual_block.py
@@ -1,30 +1,38 @@
 import torch
 import torch.nn as nn
-from torch.autograd import Variable
-import numpy as np
-from torch.distributions import Normal, Independent, kl
-import torchvision
-import torchvision.transforms as T
-from torch.distributions import Normal, Independent, kl, MultivariateNormal
-
 
 class ResidualBlock(nn.Module):
-    def __init__(self, in_channels, stride=1):
+    def __init__(self, in_channels, out_channels=None, stride=1, downsample=None):
         super(ResidualBlock, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, in_channels, kernel_size=3, stride=stride, padding=1, bias=True, dilation = 1)
-        self.bn1 = nn.BatchNorm2d(in_channels)
-        self.relu1 = nn.ReLU()
-        self.relu2 = nn.ReLU()
-        self.conv2 = nn.Conv2d(in_channels, in_channels, kernel_size=3, stride=stride, padding=1, bias=True, dilation = 1)
-        self.bn2 = nn.BatchNorm2d(in_channels)
-        
+        self.out_channels = out_channels if out_channels is not None else in_channels
+        self.conv1 = nn.Conv2d(in_channels, self.out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(self.out_channels)
+        self.relu = nn.ReLU()
+        self.conv2 = nn.Conv2d(self.out_channels, self.out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(self.out_channels)
+        self.downsample = downsample
+
     def forward(self, x):
-        residual = x
+        identity = x
+
         out = self.conv1(x)
         out = self.bn1(out)
-        out = self.relu1(out)
+        out = self.relu(out)
+
         out = self.conv2(out)
         out = self.bn2(out)
-        out += residual
-        out = self.relu2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
         return out
+
+# Example usage of ResidualBlock with downsampling
+# downsample = nn.Sequential(
+#     nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
+#     nn.BatchNorm2d(out_channels)
+# )
+# block = ResidualBlock(in_channels, out_channels, stride, downsample)
